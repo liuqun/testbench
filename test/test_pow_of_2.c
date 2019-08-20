206d41300d3e8db66088aaee47d528295106a72a
Liu Qun <517067180@qq.com>
2019-08-20 17:12:53 +0800

    格式化代码风格为4空格对齐

--- test/test_pow_of_2.c
+++ test/test_pow_of_2.c
@@ -1,3 +1,9 @@
+/*
+代码风格要求:
+1. 为了与Qt开发环境风格一致, 要求调整eclipse代码对齐方式, 将所有tab展开并替换为4个空格.
+2. (待定)
+*/
+
 #include "unity.h"
 #include "pow_of_2.h"
 
@@ -11,7 +17,7 @@ void tearDown(void)
 
 void test_math_func_(void)
 {
-	TEST_ASSERT_FALSE_MESSAGE(is_pow_of_2(0), "注意:0不是2的整数幂次方!!");
+    TEST_ASSERT_FALSE_MESSAGE(is_pow_of_2(0), "注意:0不是2的整数幂次方!!");
     TEST_ASSERT_TRUE(is_pow_of_2(1));
     TEST_ASSERT_TRUE(is_pow_of_2(2));
     TEST_ASSERT_TRUE(is_pow_of_2(4));
