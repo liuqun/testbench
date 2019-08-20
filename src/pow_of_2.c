2f19e87a9109b9587ca60137d6e57c1ca4f1b7cb
Liu Qun <517067180@qq.com>
2019-08-20 17:26:24 +0800

    临时删掉src子目录和test下的测试用例
    Signed-off-by: Liu Qun <517067180@qq.com>

--- src/pow_of_2.c
+++
@@ -1,6 +0,0 @@
-#include "pow_of_2.h"
-
-int is_pow_of_2(unsigned int num)
-{
-    return num && !(num & num - 1);
-}
--- src/pow_of_2.h
+++
@@ -1,6 +0,0 @@
-#ifndef _POW_OF_2_H
-#define _POW_OF_2_H
-
-int is_pow_of_2(unsigned int num);
-
-#endif // _POW_OF_2_H
